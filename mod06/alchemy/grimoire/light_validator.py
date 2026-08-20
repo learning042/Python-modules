fcbff0e8adeb2a62fcfcccac189093bb7ab51597
learning042 <individualthinking1403@gmail.com>
2026-08-20 01:23:49 +0100

    update

--- mod06/alchemy/grimoire/light_validator.py
+++ mod06/alchemy/grimoire/light_validator.py
@@ -1,9 +1,6 @@
-from importlib import import_module
-
-
 def validate_ingredients(ingredients: str) -> str:
-    module = import_module(".light_spellbook", package=__package__)
-    allowed_ingredients = module.light_spell_allowed_ingredients()
+    from .light_spellbook import light_spell_allowed_ingredients
+    allowed_ingredients = light_spell_allowed_ingredients()
     lower = ingredients.lower()
     if any(ingredient in lower for ingredient in allowed_ingredients):
         return f"{ingredients} - VALID"
