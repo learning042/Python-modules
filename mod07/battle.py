9f80bf837f4110215ed3a621a4a5cd2827ff52b5
learning042 <individualthinking1403@gmail.com>
2026-08-25 22:24:57 +0100

    finished mod07

--- mod07/battle.py
+++ mod07/battle.py
@@ -2,38 +2,45 @@ from ex0 import CreatureFactory, FlameFactory, AquaFactory
 
 
 def test_factory(factory: CreatureFactory) -> None:
-    print(" Testing factory")
+    print("Testing factory")
     base = factory.create_base()
     evolved = factory.create_evolved()
     print(base.describe())
     print(base.attack())
     print(evolved.describe())
     print(evolved.attack())
+    print()
 
 
-def test_battle(factory1: CreatureFactory, factory2: CreatureFactory) -> None:
-    print(" Testing battle")
-    base1 = factory1.create_base()
-    base2 = factory2.create_base()
-    print(base1.describe())
-    print("  vs.")
-    print(base2.describe())
-    print("  fight!")
-    print(base1.attack())
-    print(base2.attack())
+def battle(
+            factory1: CreatureFactory,
+            factory2: CreatureFactory,
+            evolution1: str = "base",
+            evolution2: str = "base"
+           ) -> None:
+    print("Testing battle")
+    for ev in (evolution1, evolution2):
+        if ev not in ("base", "evolved"):
+            raise ValueError(
+                    "The creature must be 'base' or 'evolved' "
+                    f", got '{ev}'"
+                   )
+    creature1 = getattr(factory1, "create_" + evolution1)()
+    creature2 = getattr(factory2, "create_" + evolution2)()
+    print(creature1.describe())
+    print(" vs.")
+    print(creature2.describe())
+    print(" fight!")
+    print(creature1.attack())
+    print(creature2.attack())
 
 
 def main() -> None:
-    try:
-        FlameFac = FlameFactory()
-        AquaFac = AquaFactory()
-        test_factory(FlameFac)
-        print()
-        test_factory(AquaFac)
-        print()
-        test_battle(FlameFac, AquaFac)
-    except Exception as e:
-        print(f"Error: {e}")
+    factory1 = FlameFactory()
+    factory2 = AquaFactory()
+    test_factory(factory1)
+    test_factory(factory2)
+    battle(factory1, factory2)
 
 
 if __name__ == "__main__":
--- mod07/ex0/_creatures.py
+++ mod07/ex0/_creatures.py
@@ -1,33 +1,34 @@
 from .creatures import Creature
+from typing import Any
 
 
 class Flameling(Creature):
-    def __init__(self) -> None:
-        super().__init__("Flameling", "Fire")
+    def __init__(self, **kwargs: Any) -> None:
+        super().__init__("Flameling", "Fire", **kwargs)
 
     def attack(self) -> str:
-        return f" {self._name} uses Ember!"
+        return f"{self._name} uses Ember!"
 
 
 class Pyrodon(Creature):
-    def __init__(self) -> None:
-        super().__init__("Pyrodon", "Fire/Flying")
+    def __init__(self, **kwargs: Any) -> None:
+        super().__init__("Pyrodon", "Fire/Flying", **kwargs)
 
     def attack(self) -> str:
-        return f" {self._name} uses Flamethrower!"
+        return f"{self._name} uses Flamethrower!"
 
 
 class Aquabub(Creature):
-    def __init__(self) -> None:
-        super().__init__("Aquabub", "Water")
+    def __init__(self, **kwargs: Any) -> None:
+        super().__init__("Aquabub", "Water", **kwargs)
 
     def attack(self) -> str:
-        return f" {self._name} uses Water Gun!"
+        return f"{self._name} uses Water Gun!"
 
 
 class Torragon(Creature):
-    def __init__(self) -> None:
-        super().__init__("Torragon", "Water")
+    def __init__(self, **kwargs: Any) -> None:
+        super().__init__("Torragon", "Water", **kwargs)
 
     def attack(self) -> str:
-        return f" {self._name} uses Hydro Pump!"
+        return f"{self._name} uses Hydro Pump!"
